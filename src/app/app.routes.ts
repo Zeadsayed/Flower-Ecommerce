884999774dbaf4cd26c8eda53d45d3fcb757d1d6
Nesma Fayz <138986422+nesmafayz@users.noreply.github.com>
2025-01-19 21:05:12 +0200

    make global styles and routes of home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './core/layout/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: MainLayoutComponent,
+    children:[
+        {path:'', pathMatch:'full', canActivate:[],
+            loadChildren: ()=>
+                import('./feature/pages/home/home.route').then((m)=>m.routes)
+        },
+        {path:'home', canActivate:[],
+            loadChildren: ()=>
+                import('./feature/pages/home/home.route').then((m)=>m.routes)        }
+
+        ]
+    }
+];
---
+++ src/app/feature/pages/home/home.route.ts
@@ -0,0 +1,15 @@
+import {Routes } from "@angular/router";
+
+export const routes: Routes = [
+    {
+      path: '',
+      loadComponent: () =>
+        import('./home-main/home-main.component').then((m) => m.HomeMainComponent),
+      data: {
+        title: 'Home',
+      }
+
+    }
+  ];
+
+  
