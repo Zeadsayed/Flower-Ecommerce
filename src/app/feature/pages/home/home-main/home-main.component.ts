de977d1f49eae668fb97703b4cacb1ceb33556a2
Seif Emam <seifmoataz27249@gmail.com>
2025-01-29 19:58:06 +0200

    done

--- src/app/feature/pages/home/home-main/home-main.component.ts
+++ src/app/feature/pages/home/home-main/home-main.component.ts
@@ -4,10 +4,11 @@ import { CategoriesComponent } from './categories/categories.component';
 import { DeliveryInfoComponent } from "./delivery-info/delivery-info.component";
 import { TrustCompaniesComponent } from "./trust-companies/trust-companies.component";
 import { SpecialGiftsComponent } from './special-gifts/special-gifts.component';
+import { BestSellerComponent } from './best-seller/best-seller.component';
 
 @Component({
   selector: 'app-home-main',
-  imports: [CategoriesComponent, PopularItemsComponent, DeliveryInfoComponent, TrustCompaniesComponent,SpecialGiftsComponent],
+  imports: [CategoriesComponent, PopularItemsComponent, DeliveryInfoComponent, TrustCompaniesComponent,SpecialGiftsComponent,BestSellerComponent],
   templateUrl: './home-main.component.html',
   styleUrl: './home-main.component.scss',
 })
