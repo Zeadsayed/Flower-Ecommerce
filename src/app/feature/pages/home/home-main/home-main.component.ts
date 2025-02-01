ccc6e52093ccab6f99894a4183e6bee395ef784d
Seif Emam <seifmoataz27249@gmail.com>
2025-02-01 12:58:51 +0200

    Done Gallary Feature

--- src/app/feature/pages/home/home-main/home-main.component.ts
+++ src/app/feature/pages/home/home-main/home-main.component.ts
@@ -4,10 +4,11 @@ import { CategoriesComponent } from './categories/categories.component';
 import { DeliveryInfoComponent } from "./delivery-info/delivery-info.component";
 import { TrustCompaniesComponent } from "./trust-companies/trust-companies.component";
 import { SpecialGiftsComponent } from './special-gifts/special-gifts.component';
+import { GallaryComponent } from './gallary/gallary.component';
 
 @Component({
   selector: 'app-home-main',
-  imports: [CategoriesComponent, PopularItemsComponent, DeliveryInfoComponent, TrustCompaniesComponent,SpecialGiftsComponent],
+  imports: [CategoriesComponent, PopularItemsComponent, DeliveryInfoComponent, TrustCompaniesComponent,SpecialGiftsComponent,GallaryComponent],
   templateUrl: './home-main.component.html',
   styleUrl: './home-main.component.scss',
 })
