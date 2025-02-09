d3caaee75385744e5e8c3ad69d5d9e442740d3dc
Seif Emam <seifmoataz27249@gmail.com>
2025-02-09 11:25:57 +0200

    Sidbar Done

--- src/app/feature/pages/category/sidebar-category/sidebar-category.component.ts
+++ src/app/feature/pages/category/sidebar-category/sidebar-category.component.ts
@@ -19,19 +19,38 @@ export class SidebarCategoryComponent {
 
   categories = [
     { id: 'cat1', name: 'Home & Living', count: 8 },
-    { id: 'cat2', name: 'Groom Care', count: 8 },
+    { id: 'cat2', name: 'Garment Care', count: 8 },
+    { id: 'cat3', name: 'Jewelry & Accessories', count: 8 },
+    { id: 'cat4', name: 'Occasion Gifts', count: 8 },
+
+    { id: 'cat5', name: 'Office & Stationery', count: 8 },
+    { id: 'cat6', name: 'Personalised Gifts', count: 8 },
+
+    { id: 'cat7', name: 'Gifts Box', count: 8 },
+
+
     // ... add other categories
   ];
 
   brands = [
     { id: 'brand1', name: 'Trooba', count: 8 },
     { id: 'brand2', name: 'Sundrey', count: 8 },
+    { id: 'brand3', name: 'Sahoo Gifts', count: 8 },
+
+    { id: 'brand4', name: 'Casterly', count: 8 },
+
+    { id: 'brand5', name: 'Mainden Gifts', count: 8 },
+
     // ... add other brands
   ];
 
   sales = [
     { id: 'sale1', name: 'On Sale', count: 8 },
     { id: 'sale2', name: 'In Stock', count: 8 },
+    { id: 'sale2', name: 'Out Of Stock', count: 8 },
+
+    { id: 'sale4', name: 'Discount', count: 8 },
+
     // ... add other sales options
   ];
 
@@ -62,5 +81,4 @@ export class SidebarCategoryComponent {
   toggleSidebar() {
     this.isOpen = !this.isOpen;
     // Toggle body class to prevent scrolling when sidebar is open
-    document.body.classList.toggle('sidebar-open', this.isOpen);
   }  }
