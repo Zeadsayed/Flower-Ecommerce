11175c38619542b7e0aa8dc6e0eb617620e7c682
zead sayed <zead.sayed@oasisoft.net>
2025-02-13 10:27:02 +0200

    [init] about page

--- src/app/feature/pages/about/about.component.ts
+++ src/app/feature/pages/about/about.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { DeliveryInfoComponent } from '../home/home-main/delivery-info/delivery-info.component';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [DeliveryInfoComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
