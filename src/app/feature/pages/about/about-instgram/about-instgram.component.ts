d5e4048ed971e2b49268ed142eff8e5bf86be0ec
Khalid Ahmed Ahmed <khalid.ahmed@oasisoft.net>
2025-02-19 12:08:06 +0200

    [deployment]

--- src/app/feature/pages/about/about-instgram/about-instgram.component.ts
+++ src/app/feature/pages/about/about-instgram/about-instgram.component.ts
@@ -5,13 +5,14 @@ import { Component } from '@angular/core';
   selector: 'app-about-instgram',
   imports: [NgFor],
   templateUrl: './about-instgram.component.html',
-  styleUrl: './about-instgram.component.scss'
+  styleUrl: './about-instgram.component.scss',
 })
 export class AboutInstgramComponent {
   images = [
-    '/icons/image 42.svg',
-    '/icons/image 43.svg',
-    '/icons/image 44.svg',
-    '/icons/image 45.svg',
-    '/icons/image 46.svg'
-  ]; }
+    'icons/image 42.svg',
+    'icons/image 43.svg',
+    'icons/image 44.svg',
+    'icons/image 45.svg',
+    'icons/image 46.svg',
+  ];
+}
