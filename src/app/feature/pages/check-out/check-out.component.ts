f39d48389e092f7b280f30162b7d21e0ff3da178
Zeadsayed <zeadsayed5@gmail.com>
2025-02-23 15:43:39 +0200

    [finish] cart summary

--- src/app/feature/pages/check-out/check-out.component.ts
+++ src/app/feature/pages/check-out/check-out.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CheckoutService } from '../../services/checkout/checkout.service';
 import { CartSummaryComponent } from '../../../shared/components/ui/cart-summary/cart-summary.component';
+import { ICart, IUserCart } from '../../../core/interfaces/checkout/Cart';
 
 @Component({
   selector: 'app-check-out',
@@ -10,14 +11,20 @@ import { CartSummaryComponent } from '../../../shared/components/ui/cart-summary
   styleUrl: './check-out.component.scss',
 })
 export class CheckOutComponent {
+  summaryData!: ICart;
   private _checkoutService = inject(CheckoutService);
   ngOnInit() {
     this.getUserCart();
   }
 
   getUserCart() {
-    this._checkoutService.getUserCart().subscribe((data) => {
-      console.log(data);
-    });
+    this._checkoutService.getUserCart().subscribe(
+      (res) => {
+        this.summaryData = res.cart;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 }
--- src/app/feature/services/checkout/checkout.service.ts
+++ src/app/feature/services/checkout/checkout.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { ApiRoutes } from '../../../core/interfaces/apiRoutes';
 import { Observable } from 'rxjs';
+import { IUserCart } from '../../../core/interfaces/checkout/Cart';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,7 @@ export class CheckoutService {
 
   constructor(private http: HttpClient) {}
 
-  getUserCart(): Observable<any> {
-    return this.http.get<any>(this.env + ApiRoutes.checkout.cart);
+  getUserCart(): Observable<IUserCart> {
+    return this.http.get<IUserCart>(this.env + ApiRoutes.checkout.cart);
   }
 }
