321d5766c16040570d6ec6e8eb35880522e7d309
Khalid Ahmed Ahmed <khalid.ahmed@oasisoft.net>
2025-02-24 16:01:32 +0200

    [update] design cart

--- src/app/feature/pages/check-out/cart/cart.component.ts
+++ src/app/feature/pages/check-out/cart/cart.component.ts
@@ -4,8 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-cart',
   imports: [],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrl: './cart.component.scss',
 })
 export class CartComponent {
+  quantity: number = 1;
 
+  increaseQuantity(): void {
+    this.quantity++;
+  }
+
+  decreaseQuantity(): void {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
 }
